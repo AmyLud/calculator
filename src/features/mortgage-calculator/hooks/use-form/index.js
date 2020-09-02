9acaeeb282f94980b5b2bd7d4cb08db4087edc41
Amy <amy_ludwig@live.com>
2020-09-01 20:46:19 -0400

    create hooks for form

---
+++ src/features/mortgage-calculator/hooks/use-form/index.js
@@ -0,0 +1,54 @@
+import { useState, useEffect, useRef } from "react";
+
+const useForm = ({ initialValues, onSubmit }) => {
+  const [values, setValues] = useState(initialValues || {});
+  const [errors, setErrors] = useState({});
+  const [touched, setTouched] = useState({});
+  const [submitting, setSubmitting] = useState(false);
+
+  const formRendered = useRef(true);
+
+  useEffect(() => {
+    if (!formRendered.current) {
+      setValues(initialValues);
+      setErrors({});
+      setTouched({});
+      setSubmitting(false);
+    }
+    formRendered.current = false;
+  }, [initialValues]);
+
+  const handleChange = (event) => {
+    const { target } = event;
+    const { name, value } = target;
+    event.persist();
+    if (!value) {
+      setErrors({ ...errors, [name]: "error" });
+    } else {
+      setErrors({ ...errors, [name]: null });
+    }
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    if (event) {
+      event.persist();
+      event.preventDefault();
+    }
+    setSubmitting(true);
+    setErrors({ ...errors });
+    onSubmit({ values, errors });
+    setSubmitting(false);
+  };
+
+  return {
+    values,
+    errors,
+    touched,
+    handleChange,
+    submitting,
+    handleSubmit,
+  };
+};
+
+export default useForm;
