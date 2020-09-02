88de9773fb6cf874dfc3982c05cb4c255da4ac4e
Amy <amy_ludwig@live.com>
2020-09-01 20:47:14 -0400

    add helper for getting select options

---
+++ src/features/mortgage-calculator/helpers/get-options/index.js
@@ -0,0 +1,49 @@
+import messages from "./messages";
+
+export const getAmorOptions = (start = 1, end = 30) => {
+  let i = end;
+  let options = [];
+  while (i > start - 1) {
+    options.push({
+      value: i,
+      text: `${i} ${messages.years}`,
+      selected: i === 25 && "selected",
+    });
+    i--;
+  }
+  return options;
+};
+
+export const getTermOptions = (start = 1, end = 10) => {
+  let i = end;
+  let options = [];
+  while (i > start - 1) {
+    options.push({
+      value: i,
+      text: `${i} ${messages.years}`,
+      selected: i === 5 && "selected",
+    });
+    i--;
+  }
+  return options;
+};
+
+export const payFrequencies = [
+  {
+    value: 12,
+    text: messages.monthly,
+    selected: "selected",
+  },
+  {
+    value: 24,
+    text: messages.semi,
+  },
+  {
+    value: 26,
+    text: messages.biWeek,
+  },
+  {
+    value: 52,
+    text: messages.week,
+  },
+];
